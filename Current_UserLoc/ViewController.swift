a74900f1283edaba0e89fcdcba623057431b6a18
mister springroll <misterspringroll@123qwes-mbp>
2018-03-12 14:39:42 -0700

    Initial Commit

---
+++ Current_UserLoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Current_UserLoc
+//
+//  Created by mister.springroll on 2018/3/12.
+//  Copyright © 2018年 mister.springroll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
